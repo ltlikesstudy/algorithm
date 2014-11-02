8296e1105a7dfde43bbcc6949e774b59ef7e8ccf
Linda <ltlikesstudy@aol.com>
2014-11-01 20:41:01 -0700

    1
    1

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by apple on 14-10-13.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
